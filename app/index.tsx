1ef5b47dbe2e1be50a955e5c07c1cc97972efa2f
pepanzdorf <melicharovo@gmail.com>
2024-07-06 16:50:09 +0200

    fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
 
-export default function Index() {
+import React, { useEffect, useState } from "react";
+import {View, Text, FlatList} from 'react-native';
+
+export default function App(){
+  const [users,setUsers] = useState([]);
+
+  useEffect(()=>{
+    fetchData()
+  },[]);
+
+  const fetchData = () => {
+    fetch("http://192.168.1.113:5000/climbing/boulders")
+      .then(response => response.json())
+      .then(jsonResponse => setUsers(jsonResponse))
+      .catch(error => console.log(error))
+  };
+
+  const renderUser = ({item}) => {
+    return (
+      <View style={{margin:10,borderWidth:0.5,padding:10}}>
+        <Text style={{color:"black",fontSize:16,fontWeight:"bold"}}>
+          User {item.id}
+        </Text>
+        <Text style={{color:"black"}}>grade : {item.grade}</Text>
+        <Text style={{color:"black"}}>name : {item.name}</Text>
+        <Text style={{color:"black"}}>angle : {item.angle}</Text>
+      </View>
+    )
+  }
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{flex:1,backgroundColor:"white"}}>
+      <FlatList
+        data={users.sort((a,b) => a.name.localeCompare(b.name))}
+        renderItem={renderUser}
+        />
     </View>
-  );
+  )
 }

2f2a2d01c0310ffed2e6f67f085933611d9616a6
pepanzdorf <melicharovo@gmail.com>
2024-11-23 12:26:27 +0100

    competition page

--- app/(tabs)/(boulders)/_layout.tsx
+++ app/(tabs)/(boulders)/_layout.tsx
@@ -6,6 +6,7 @@ export default function BoulderStackLayout() {
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="[id]" options={{ headerShown: false }} />
       <Stack.Screen name="sends/[id]" options={{ headerShown: false }} />
+      <Stack.Screen name="comps/[id]" options={{ headerShown: false }} />
     </Stack>
   );
 }
